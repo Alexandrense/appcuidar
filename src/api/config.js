a8c28979f95d2e833007b99aa65abb38ce1a14ca
Alexandre Carvalho <alexbh@gmail.com>
2023-04-16 21:27:48 +0100

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.API_URL;
+const API_URL = process.env.APPCUIDAR_API_URL;
 //const API_URL = "https://appcuidar-api.herokuapp.com";
 //const API_URL = "http://localhost:8080";
 export default API_URL;
